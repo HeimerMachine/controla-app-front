b06c6f9cf114747830f5ad0f3d50aac2cb196927
Beatriz Alves dos Santos <104687501+Bealvs@users.noreply.github.com>
2025-06-05 17:12:56 -0300

    Feat/beatriz/#2 (#13)
    * chore: update dependencies and add new packages
    
    * feat: add Toaster component for notifications
    
    * feat: implement user registration and login functionality with API integration

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,18 +1,56 @@
 import "./Login.css";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { api } from "../../../lib/api";
+import Cookies from "js-cookie"
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router";
+
+
+interface FormState {
+  email: string;
+  password: string;
+}
 
 export function Login() {
+
+    const navigate = useNavigate();
+    
+  const [form, setForm] = useState<FormState>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    console.log(form);
+    try{
+      const response = await api.post(`users/login/`, form)
+      Cookies.set("token", response.data.token, {expires: 1/2})
+      toast.success("Login efetuado com sucesso!")
+     navigate("/")
+    }
+    catch (error){
+      toast.error("Houve um problema ao enviar os seus dados!")
+    }
+  };
+
+
     return (
         <div className="container_login">
-            <form className="form_login">
+            <form onSubmit={handleSubmit} className="form_login">
                 <img className="logo_controla" src="src\assets\logo-controla.svg" alt="logo_controla" />
                 <h2>Faça login na sua conta!</h2>
                 <div className="input_container">
                     <label>Email:</label>
-                    <input type="email" placeholder="Digite o seu email" />
+                    <input value={form.email} name="email" onChange={handleChange} type="email" placeholder="Digite o seu email" required/>
                 </div>
                 <div className="input_container">
                     <label>Senha:</label>
-                    <input type="password" placeholder="Digite a sua senha" />
+                    <input value={form.password} name="password" onChange={handleChange} type="password" placeholder="Digite a sua senha" required/>
                     <a href="" className="link_password">Esqueci a senha.</a>
                 </div>
                 <button className="button_login" >Entrar</button>
